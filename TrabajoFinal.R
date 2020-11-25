2372bb4e63b885d87766a6ed4b5c1ff226f0af55
Lenovo <christian_joseph7@hotmail.com>
2020-11-25 09:43:30 -0500

    Trabajo Finalv2

--- TrabajoFinal.R
+++ TrabajoFinal.R
@@ -596,3 +596,23 @@ regresion<-function(x,y, nx=NA, ny=NA){
 regresion(paraKNN$porcentaje_profes,paraKNN$porcentaje_adoles,nx=14)
 plot(paraKNN$porcentaje_profes,paraKNN$porcentaje_adoles)
 #######################################################
+#Exportar archivos a excel- LA TABLA PROFESIONAL
+
+write.csv(qdivision_profesionales_dep, file = "porcentaje_adolescentes.csv")
+
+#Envio de correos 
+
+
+library(rJava)
+library(mailR)
+
+send.mail(from = "farmaciacercana2020@gmail.com",
+          to = c("christian_joseph7@hotmail.com"), # Colocar correos
+          subject = "Test de envío email con R",
+          body = "Esta es una prueba para envíos de correo con R",
+          smtp = list(host.name = "smtp.gmail.com", port = 465,
+                      user.name = "farmaciacercana2020@gmail.com",
+                      passwd = "universidad2020", ssl = TRUE),  # Cambiar contraseña
+          authenticate = TRUE,
+          send = TRUE)
+
